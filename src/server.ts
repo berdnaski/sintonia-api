e1522a497c2daf9eeba5454da74c5bf965e96c54
berdnaski <erick@vendbot.com.br>
2025-02-20 22:55:29 -0300

    chore: added cors in project

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import 'dotenv/config';
 import { fastify, type FastifyInstance } from "fastify";
+import cors from "@fastify/cors"
 
 const app: FastifyInstance = fastify();
+
+app.register(cors, {
+  origin: process.env.HOST,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization']
+})
 
 app.listen(
   {
