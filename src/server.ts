e191836eed8e3e5afe33268b2edfc90c260a6302
Julio Developer <juliodeveloper777@gmail.com>
2025-02-22 16:27:19 -0300

    fix: server

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,26 @@
+import fastifyCors from '@fastify/cors';
+import fastifyFormbody from '@fastify/formbody';
 import 'dotenv/config';
 import { fastify, type FastifyInstance } from "fastify";
-import cors from "@fastify/cors"
-import { authRoutes } from './routes/authRoutes';
-import fastifyJwt from "fastify-jwt"
+import fastifyJwt from "fastify-jwt";
 import { errorHandler } from './middlewares/error-handler';
+import { authRoutes } from './routes/authRoutes';
 import { userRoutes } from './routes/userRoutes';
 
 const app: FastifyInstance = fastify();
 
-// app.register(cors, {
-//   origin: process.env.HOST,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true,
-//   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization']
-// })
-
 app.register(fastifyJwt, {
-  secret: process.env.JWT_SECRET!,
+  secret: process.env.SECRET_KEY as string,
   sign: {
-    expiresIn: '1h',
+    expiresIn: '1d',
   }
 })
 
-app.register(authRoutes, { prefix: 'auth' });
-app.register(userRoutes);
-
 app.setErrorHandler(errorHandler);
+app.register(fastifyCors)
+app.register(fastifyFormbody)
+app.register(userRoutes);
+app.register(authRoutes, { prefix: 'auth' });
 
-app.listen(
-  {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-  },
-  () => console.log('listening on port ' + process.env.PORT)
-)
+const PORT = Number(process.env.PORT) || 3000
+app.listen({ port: PORT }, () => console.log(`listening on port ${PORT}`))
