6971537f8d19dc181b55871b80b065f0dca9622f
Julio Developer <juliodeveloper777@gmail.com>
2025-02-22 16:28:16 -0300

    fix: router

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,13 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import { UserController } from "../controllers/user-controller";
 import type { UserUpdate } from "../interfaces/user.interface";
-import { auth } from "../middlewares/auth";
-
+import { Auth } from "../middlewares/auth";
 
 export async function userRoutes(app: FastifyInstance) {
   const userController = new UserController(app);
 
-  app.addHook("onRequest", auth);
+  app.addHook("onRequest", Auth);
 
   app.get('/users', async (req, reply) => {
     await userController.findAll(req, reply);
